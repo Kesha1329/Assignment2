746455f690991f44bd5d5c6217b3b6ade96aac53
Kesha Devkar <91537119+Kesha1329@users.noreply.github.com>
2021-10-09 16:30:22 +0000

    os5

--- ComputerOS.js
+++ ComputerOS.js
@@ -1,6 +1,6 @@
 var os = require('os');
 console.log("CPU Architecture: " + os.arch());
-console.log("Free Memory: " + os.freemem()/(1024 * 1024 * 1024));
-console.log("Total Memory: " + os.totalmem()/(1024 * 1024 * 1024));
+console.log("Free Memory: " + os.freemem()/(1024 * 1024 * 1024 *1024));
+console.log("Total Memory: " + os.totalmem()/(1024 * 1024 * 1024 * 1024));
 console.log("Platform: " + os.platform());
 console.log("About Current User: " + os.userInfo());
